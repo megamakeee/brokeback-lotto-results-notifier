5a6be84f7093f9637a3821dbfaefa364a34b3686
Marko Tikkanen <m.tikkanen@iki.fi>
2020-11-21 17:59:15 +0200

    Created new function and changed the return data on get_latest_lotto_results function.

--- lotto.py
+++ lotto.py
@@ -9,6 +9,7 @@ import requests
 import json
 import time
 import datetime
+from csv import reader
 
 """
     Function to get the latest lotto results
@@ -40,13 +41,37 @@ def get_latest_lotto_results():
 
     if r.status_code == 200:
         if len(r.json()) > 0:
-            return r.json()[0]
+            return r.json()[0]["results"][0]["primary"] + r.json()[0]["results"][0]["secondary"] + r.json()[0]["results"][0]["tertiary"]
         else:
             raise Exception("Empty result set", 69)
     else:
         raise Exception("API query failed", r.status_code)
 
 
-lotto_results = get_latest_lotto_results()
+"""
+    Function to read own lottery numbers to list of lists
 
+"""
+
+
+def get_own_lotto_lines_to_list_of_lists(filename):
+    # read csv file as a list of lists
+    with open(filename, 'r') as read_obj:
+        # Pass the file object to reader() to get the reader object
+        csv_reader = reader(read_obj)
+        # Pass reader object to list() to get a list of lists
+        list_of_rows = list(csv_reader)
+        return list_of_rows
+
+
+# The Script #
+
+# Basic variables
+numbers_file = "numbers.csv"
+
+# Process itself
+lotto_results = get_latest_lotto_results()
 print(lotto_results)
+
+lotto_lines = get_own_lotto_lines_to_list_of_lists(numbers_file)
+print(lotto_lines)
