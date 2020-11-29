12dff19f370efda0aaaddc46fbdb4c39200bad0f
Marko Tikkanen <m.tikkanen@iki.fi>
2020-11-29 18:51:27 +0200

    First fully functioning version of the script and functions!

--- lotto.py
+++ lotto.py
@@ -5,24 +5,41 @@
 """
 
 import lottoFunctions as lf
+import os
 
 # The Script #
 
 # Basic variables
-numbers_file = "numbers.csv"
+lotto_numbers_file = "numbers.csv"
+phone_numbers_file = "phonenumbers.csv"
 
 # Process itself
 
 # Get the current draw
 current_draw = lf.get_latest_lotto_draw()
-print(current_draw)
+# print(current_draw)
+
+# Get the prize tiers
+prizeTiers = lf.get_prize_tiers()
+# print(prizeTiers)
 
 # Get your own played lines
-played_lines = lf.get_own_lotto_lines_to_list_of_sets(numbers_file)
-print(played_lines)
+played_lines = lf.get_own_lotto_lines_to_list_of_sets(lotto_numbers_file)
+# print(played_lines)
+
+# Get phonenumbers
+phonenumbers = lf.get_phonenumbers(phone_numbers_file)
+# print(phonenumbers)
 
 # Check the results
 results = lf.check_results(current_draw, played_lines)
-print(results)
+# print(results)
 
-# If there are wins get the prizes
+# Formulate the message
+message = lf.formulate_message(results, prizeTiers)
+# print(message)
+
+# Send the formulated message of weeks results to the phonenumbers in phone_numbers_file
+for phonenumber in phonenumbers:
+    print(f"lf.send_sms({phonenumber[0]},{os.environ.get('twilio_from_number')},\"{message}\")")
+    lf.send_sms(phonenumber[0], os.environ.get('twilio_from_number'), message)
--- lottoFunctions.py
+++ lottoFunctions.py
@@ -18,7 +18,7 @@ def get_latest_lotto_draw():
     endpointURL = "/api/draw-results/v1/games/LOTTO/draws/by-week/"
     year = str(datetime.date.today().isocalendar()[0])
     # Week number has -1 to get some results whenever script is run
-    week = str(datetime.date.today().isocalendar()[1] - 1)
+    week = str(datetime.date.today().isocalendar()[1])
     api_url = baseURL + endpointURL + year + "-W" + week
 
     # Set headers
@@ -114,4 +114,83 @@ def check_results(current_draw, played_lines):
             result = f"{correct_primary_numbers}"
             results.append(result)
 
+    results.sort()
     return results
+
+
+def formulate_message(results, prizes):
+    message = f"BROKEBACK LOTTO\n"
+    if len(results) > 0:
+        message += f"Hyvä jätkät, hienosti!\nTällä viikolla kupongissa oli "
+        counter = 1
+        winnings = 0
+        for result in results:
+            winnings += prizes[result]
+            if counter == len(results[:-1]) and len(results) > 1:
+                message += f"{result} "
+            elif counter < len(results) and len(results) > 1:
+                message += f"{result}, "
+            elif counter == len(results) and len(results) > 1:
+                message += f"ja {result}"
+            elif counter == len(results) and len(results) == 1:
+                message += f"{result}"
+            counter += 1
+        winnings = str(winnings).replace(".", ",")
+        message += f" oikein.\nVoititte yhteensä {winnings} euroa!"
+    else:
+        message += "Pirskatti! Ei voittoa lotossa tällä viikolla."
+
+    return message
+
+
+def get_prize_tiers():
+    # Set host
+    baseURL = "https://www.veikkaus.fi"
+    endpointURL = "/api/draw-results/v1/games/LOTTO/draws/by-week/"
+    year = str(datetime.date.today().isocalendar()[0])
+    # Week number has -1 to get some results whenever script is run
+    week = str(datetime.date.today().isocalendar()[1])
+    api_url = baseURL + endpointURL + year + "-W" + week
+
+    # Set headers
+    headers = {
+            'Content-type': 'application/json',
+            'Accept': 'application/json',
+            'X-ESA-API-Key': 'ROBOT'
+    }
+
+    # Create a session
+    s = requests.Session()
+
+    # Get the lotto results
+    r = s.get(api_url, headers=headers)
+
+    if r.status_code == 200:
+        if len(r.json()) > 0:
+            prizeTiers = r.json()[0]["prizeTiers"]
+            # Pick only the 6 first prize tiers, that does not include doubling or plus number prizes.
+            prizeTiers = prizeTiers[:6]
+            prizes = {}
+            for prize in prizeTiers:
+                prizes[prize["name"].replace(' oikein', '')] = prize["shareAmount"] / 100
+            return prizes
+        else:
+            raise Exception("Empty result set", 69)
+    else:
+        raise Exception("API query failed", r.status_code)
+
+
+def get_phonenumbers(filename):
+    """
+    Function to read phonenumbers to list
+
+    """
+
+    # read csv file as a list of lists
+    with open(filename, 'r') as read_obj:
+        # Pass the file object to reader() to get the reader object
+        csv_reader = reader(read_obj)
+        # Pass reader object to list() to get a list of lists
+        list_of_rows = list(csv_reader)
+
+    return list_of_rows
